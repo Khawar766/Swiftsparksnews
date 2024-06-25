2df4eac1414ecdbdca1d7470920ca6b00217da8a
Khawar Javaid <126099032+Khawar766@users.noreply.github.com>
2024-06-25 17:38:10 +0500

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
 echo "BUILD START"
+ pip install -r requirements.txt
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
- pip install -r requirements.txt
+
  echo "BUILD END 
